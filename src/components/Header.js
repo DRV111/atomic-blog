a8d26daeb90348a60cad83f27fedf9707e864cfe
DRV111 <xblackcrownx@gmail.com>
2023-12-19 10:57:03 +0300

    fix: fix imports in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import Results from './Results';
+import SearchPosts from './SearchPosts';
+
 function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
   return (
     <header>
