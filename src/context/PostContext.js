5e21ca28b14012d02a3ae4f902433f4b4aad9563
DRV111 <xblackcrownx@gmail.com>
2023-12-23 13:09:55 +0300

    refactor: create utility function

--- src/context/PostContext.js
+++ src/context/PostContext.js
@@ -1,3 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import { createRandomPost } from '../utils/createRandomPost';
 
 export const PostContext = createContext();
+
+function PostProvider() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+}
